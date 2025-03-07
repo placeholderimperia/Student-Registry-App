c34d783623b3ffe06aabfce239a0a0c06e48cc36
IMPERIAMG\User <martin.georgiev5564@outlook.com>
2025-03-07 17:27:42 +0200

    updateTitle2.0

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "The new Students Registry",
       students: students
     };
     res.render('home', model);
